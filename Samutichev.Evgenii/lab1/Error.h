33c3dabdbda377395904a73a380e5a13629a1926
zhenyatos <zhenyatos999@gmail.com>
2020-10-24 08:58:01 +0300

    Add the configuration file example

--- Samutichev.Evgenii/lab1/Error.h
+++ Samutichev.Evgenii/lab1/Error.h
@@ -1,3 +1,7 @@
 enum class Error {
-    NO_SUCH_FIELD, NO_CONFIG_FILE, CONFIG_FAIL, NO_SUCH_FOLDER, UNKNOWN
+    NO_SUCH_FIELD = 1, 
+    NO_CONFIG_FILE = 2, 
+    CONFIG_FAIL = 3, 
+    NO_SUCH_FOLDER = 4, 
+    UNKNOWN = 5
 };
