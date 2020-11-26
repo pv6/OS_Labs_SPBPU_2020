c9fcf207289dff75e87f753a0291900c89b2f8fc
DanilPestryakov <55560885+DanilPestryakov@users.noreply.github.com>
2020-11-27 00:40:01 +0500

    Added all files

---
+++ lab2/conn_seg.h
@@ -0,0 +1,10 @@
+#ifndef CONN_SEG_H_INCLUDED
+#define CONN_SEG_H_INCLUDED
+
+#include "conn.h"
+
+class ConnSeg:Conn{
+
+};
+
+#endif // CONN_SEG_H_INCLUDED
---
+++ lab2/conn_shm.h
@@ -0,0 +1,10 @@
+#ifndef CONN_SHM_H_INCLUDED
+#define CONN_SHM_H_INCLUDED
+
+#include "conn.h"
+
+class ConnShm:Conn{
+
+};
+
+#endif // CONN_SHM_H_INCLUDED
